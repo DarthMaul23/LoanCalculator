d17e19b6ccec7b8d9b6cca2b7815a6f5c3d1791e
DarthMaul23 <fand.mou@gmail.com>
2023-05-23 14:37:49 +0200

    Initial Commit

---
+++ Loan Calculator/Loan_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loan_CalculatorApp.swift
+//  Loan Calculator
+//
+//  Created by František Moucha on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Loan_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
